a3ec872a16fdd6531921ad8e2277b41543e0001d
Chuong Nguyen <chuongnh3@gmail.com>
2020-06-25 18:24:56 +0700

    Update jscdn.js

--- jscdn.js
+++ jscdn.js
@@ -1,3 +1,17 @@
+function getQueryVariable(url,variable) {
+    var query = url.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
+var currentScript = document.currentScript.src;
 console.log('hosted on github');
 console.log('current url', window.location);
-console.log('params', document.currentScript.src);
+console.log('currentScript', currentScript);
+console.log('logotype', getQueryVariable(currentScript, 'lg'));
+console.log('rh_id', getQueryVariable(currentScript, 'rh'));
